f96fa13c3659b22f235afe18df927b84854ccdcc
alessandroarezzo <alessandro.arezzo@stud.unifi.it>
2020-12-22 14:56:27 +0100

    Modified video features

--- ModelSVR.py
+++ ModelSVR.py
@@ -16,7 +16,7 @@ class ModelSVR:
         self.preliminary_clustering = preliminary_clustering  # Preliminary clustering performed
         self.classifier = None
         self.vas_sequences = None
-        self.histo_relevant_config_videos = None
+        self.desc_relevant_config_videos = None
         self.means_gmm = self.preliminary_clustering.gmm.means
         self.dict_relevant_config = {}
         self.verbose = verbose
@@ -28,21 +28,30 @@ class ModelSVR:
             self.dict_relevant_config[str(mean_gmm)] = index
             index += 1
 
-    def __generate_histo_relevant_configuration(self):
-        histo_of_videos = self.preliminary_clustering.histograms_of_videos
-        index_relevant_configuration = self.preliminary_clustering.index_relevant_configurations
-        histo_relevant_config_videos = []
-        for histo in histo_of_videos:
-            histo_relevant_config = np.zeros(len(index_relevant_configuration))
-            for config in index_relevant_configuration:
-                mean_gmm = self.means_gmm[config]
-                histo_relevant_config[self.dict_relevant_config[str(mean_gmm)]] = histo[config]
-            sum_histo_values = sum(histo_relevant_config)
-            if sum_histo_values > 0:
-                histo_relevant_config = histo_relevant_config / sum_histo_values
-            histo_relevant_config_videos.append(histo_relevant_config)
-        self.histo_relevant_config_videos = histo_relevant_config_videos
-
+    def __generate_descriptors_relevant_configuration(self):
+        if self.verbose:
+            print("---- Generate descriptors of video sequences... ----")
+        fisher_vectors = self.preliminary_clustering.fisher_vectors
+        n_videos = len(fisher_vectors)
+        num_relevant_config = len(self.preliminary_clustering.index_relevant_configurations)
+        descriptors_of_videos = []
+        idx_relevant_frame_mean = self.preliminary_clustering.index_relevant_configurations
+        idx_relevant_frame_sd = [config + len(self.means_gmm) for config in self.preliminary_clustering.index_relevant_configurations]
+        means_gmm = self.means_gmm[self.preliminary_clustering.index_relevant_configurations]
+        idx_relevant_config_mean = [self.dict_relevant_config[str(mean_gmm)] for mean_gmm in means_gmm]
+        idx_relevant_config_sd = [self.dict_relevant_config[str(mean_gmm)] + num_relevant_config for mean_gmm in means_gmm]
+        for index in range(0, n_videos):
+            current_video_fv = fisher_vectors[index][0]
+            video_descriptor = np.zeros(shape=(num_relevant_config * 2, current_video_fv.shape[2]))
+            for index_frame in range(0, current_video_fv.shape[0]):
+                frame = current_video_fv[index_frame]
+                video_descriptor[idx_relevant_config_mean] += frame[idx_relevant_frame_mean]
+                video_descriptor[idx_relevant_config_sd] += frame[idx_relevant_frame_sd]
+            if sum(video_descriptor).any() != 0:
+                video_descriptor = np.sqrt(np.abs(video_descriptor)) * np.sign(video_descriptor)
+                video_descriptor = video_descriptor / np.linalg.norm(video_descriptor, axis=(0, 1))[None, None]
+            descriptors_of_videos.append(video_descriptor)
+        self.desc_relevant_config_videos = descriptors_of_videos
 
     """Read vas index of all sequences from dataset. 
     Return a list contained the vas index of all sequences """
@@ -51,14 +60,14 @@ class ModelSVR:
             print("---- Read vas indexes for sequences in dataset... ----")
         seq_df = pd.read_csv(self.seq_df_path)
         vas_sequences = []
-        for num_video in np.arange(len(self.histo_relevant_config_videos)):
+        for num_video in np.arange(len(self.desc_relevant_config_videos)):
             vas_sequences.append(seq_df.iloc[num_video][1])
         self.vas_sequences = vas_sequences
 
     """Train SVR using fisher vectors calculated and vas indexes readed of the sequences.
     Return the trained classifier """
     def __train_SVR(self, regularization_parameter, gamma_parameter):
-        training_set_histo = np.asarray([self.histo_relevant_config_videos[i] for i in self.train_video_idx])
+        training_set_histo = np.asarray([self.desc_relevant_config_videos[i].flatten() for i in self.train_video_idx])
         training_set_vas = np.asarray([self.vas_sequences[i] for i in self.train_video_idx])
         model_svr = svm.SVR(C=regularization_parameter, gamma=gamma_parameter)
         return model_svr.fit(training_set_histo, training_set_vas)
@@ -66,25 +75,25 @@ class ModelSVR:
     def __train_SVR_maximizing_score(self, n_jobs):
         if self.verbose:
             print("---- Find parameters that minimizes mean absolute error... ----")
-        training_set_histo = np.asarray([self.histo_relevant_config_videos[i] for i in self.train_video_idx])
+        training_set_desc = np.asarray([self.desc_relevant_config_videos[i].flatten() for i in self.train_video_idx])
         training_set_vas = np.asarray([self.vas_sequences[i] for i in self.train_video_idx])
         param = {'kernel': ['rbf'], 'C': np.arange(1, 1000, 50),
                  'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
                  'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]}
         grid_result = GridSearchCV(estimator=svm.SVR(), param_grid=param, scoring='neg_mean_absolute_error',
-                             n_jobs=n_jobs).fit(training_set_histo, training_set_vas, sample_weight=self.sample_weights)
+                             n_jobs=n_jobs).fit(training_set_desc, training_set_vas, sample_weight=self.sample_weights)
         best_params = grid_result.best_params_
         return svm.SVR(kernel=best_params["kernel"], C=best_params["C"], epsilon=best_params["epsilon"],
-                          gamma=best_params["gamma"]).fit(training_set_histo, training_set_vas, sample_weight=self.sample_weights)
+                          gamma=best_params["gamma"]).fit(training_set_desc, training_set_vas, sample_weight=self.sample_weights)
 
     def __init_data_sequences(self):
-        self.__generate_histo_relevant_configuration()
+        self.__generate_descriptors_relevant_configuration()
         self.__read_vas_videos()
 
     """Performs the model training procedure based on what was done in the preliminary clustering phase"""
     def train_SVR(self, regularization_parameter=1,
                     gamma_parameter='scale', train_by_max_score=True, classifier_dump_path=None, n_jobs=1):
-        if self.histo_relevant_config_videos == None or self.vas_sequences == None:
+        if self.desc_relevant_config_videos == None or self.vas_sequences == None:
             self.__init_data_sequences()
         if self.weighted_samples:
             self.__calculate_sample_weights()
@@ -98,7 +107,7 @@ class ModelSVR:
 
 
     def calculate_rate_model(self, path_scores_parameters=None, path_scores_cm=None):
-        test_set_histo = np.asarray([self.histo_relevant_config_videos[i] for i in self.test_video_idx])
+        test_set_histo = np.asarray([self.desc_relevant_config_videos[i] for i in self.test_video_idx])
         test_set_vas = np.asarray([self.vas_sequences[i] for i in self.test_video_idx])
         sum_error = 0
         num_test_videos = test_set_histo.shape[0]
